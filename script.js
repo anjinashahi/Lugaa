b7506059c1d322b9c77a7973a59b619ed6ec3277
JohnsonBantawa <NP03CS4A220288@heraldcollege.edu.np>
2024-03-31 21:36:10 +0545

    second initial

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const overlay = document.getElementById('overlay');
-    const closeButton = document.getElementById('close-btn');
-    const loginContainer = document.querySelector('.login-container');
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
 
-    // Function to toggle the login popup
-    function toggleLoginPopup() {
-        overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
-    }
-
-    // Event listener to show/hide the login popup when the close button is clicked
-    closeButton.addEventListener('click', toggleLoginPopup);
-
-    // Event listener to hide the login popup when clicking outside the login container
-    overlay.addEventListener('click', function (event) {
-        if (event.target === overlay) {
-            toggleLoginPopup();
-        }
-    });
+registerLink.addEventListener('click', () => {
+    console.log('Register link clicked');
+    wrapper.classList.add('active');
 });
+
+loginLink.addEventListener('click', () => {
+    console.log('Login link clicked');
+    wrapper.classList.remove('active');
+});
+
